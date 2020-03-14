2aead877a888e02784bb3220399964f8dbe3e264
Хасп Элд <EldHasp@Gmail.com>
2020-03-15 00:15:39 +0300

    Добавил в общую библиотеку универсальный WPF конвертер-словарь. Теперь можно загружать словарь соответствий названий картинок ресурсам преогромно из какого-то источника. Пока для примера сделано в CB App. Можно заменить на считывание из какого-то внешнего источника настроек.

--- GClipsaBetSolution/GClipsaWPF/App.xaml.cs
+++ GClipsaBetSolution/GClipsaWPF/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using GClipsaBetModel;
+﻿using Common;
+using GClipsaBetModel;
 using GClipsaBetViewModel;
 using System;
 using System.Collections.Generic;
@@ -24,11 +25,20 @@ namespace GClipsaWPF
 
         private void App_Startup(object sender, StartupEventArgs e)
         {
+            /// Загрузка словаря соответствия полученных названий 
+            /// рисунков их названиям в ресурсах
+            Dictionary<string, string> names = new Dictionary<string, string>()
+                {
+                    {"nophoto","Image/nophoto.png" }
+                };
+            ((DictionaryKeyToValueConverter)Resources["DictionaryKeyToValueConverter"]).SetDictionary(names);
+
             ParseData model = new ParseData("Data for connection");
             ViewModel viewModel = new ViewModel(model);
 
             new MainWindow() { DataContext = viewModel }
              .Show();
         }
+
     }
 }
---
+++ GClipsaBetSolution/GClipsaWPF/NameToImageConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace GClipsaWPF
+{
+    public class NameToImageConverter : IValueConverter
+    {
+
+        Dictionary<String, String> Names = new Dictionary<string, string>()
+        {
+            {"nophoto","Image/nophoto.png" }
+        };
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Names[value.ToString()];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
